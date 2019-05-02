6b33937b882b2a4316fc70f839c4bb68e1e49960
momoNiti <60070041@kmitl.ac.th>
2019-05-02 20:22:47 +0700

    link product to manufactor

--- products/admin.py
+++ products/admin.py
@@ -5,6 +5,9 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.forms import BaseInlineFormSet
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from products.models import Dealer, Customer, FeedBack, Admin, Admin_Customer, ProductLot, Product, Manufactor
 
@@ -58,9 +61,9 @@ class ProductLotInline(admin.StackedInline):
     model = ProductLot
     extra = 1
 class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'link_to_manufactor']
     readonly_fields = ['manufactor', 'name', 'describe', 'quantity']
     inlines = [ProductLotInline]
-
     def save_related(self, request, form, formsets, change):
         super(ProductAdmin, self).save_related(request, form, formsets, change)
         product = form.instance
@@ -74,4 +77,13 @@ class ProductAdmin(admin.ModelAdmin):
         form.save_m2m()
         product.save()
 
+    def link_to_manufactor(self, obj):
+        link = reverse("admin:products_manufactor_change", args=[obj.manufactor.id])
+        return format_html('<a href="{}">{}</a>', link, obj.manufactor.name)
+    link_to_manufactor.allow_tags = True
+    link_to_manufactor.short_description = 'View Manufactor Detail'
+
 admin.site.register(Product, ProductAdmin)
+class ManufactorAdmin(admin.ModelAdmin):
+    readonly_fields = ['name', 'location', 'phone']
+admin.site.register(Manufactor, ManufactorAdmin)
