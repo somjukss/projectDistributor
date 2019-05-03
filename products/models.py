93e36b07ddf0aa1362e8ade2723ec63450e161fb
momoNiti <60070041@kmitl.ac.th>
2019-05-03 16:54:55 +0700

    product image

--- products/models.py
+++ products/models.py
@@ -50,6 +50,7 @@ class Product(models.Model):
     minimum_stock = models.IntegerField()
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    img_url = models.TextField()
     #fk
     manufactor = models.ForeignKey(Manufactor, on_delete=models.PROTECT)
     def __str__(self):
