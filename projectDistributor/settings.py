f7a99e59af4d4867342908fd3d7baf4c0526c613
somjukss <so_somjuk@hotmail.com>
2019-05-04 22:28:27 +0700

    update sql

--- projectDistributor/settings.py
+++ projectDistributor/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'project',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'somjuk123',
         'HOST': '127.0.0.1',
         'PORT': '3306'
 
